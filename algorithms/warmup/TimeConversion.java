3601623e9fb39056086a2dabcc1d8a5e1a9a72bb
Dharmendra Prasad <officialdharam@gmail.com>
2015-09-22 16:19:05 +0530

    Update TimeConversion.java

--- algorithms/warmup/TimeConversion.java
+++ algorithms/warmup/TimeConversion.java
@@ -4,7 +4,7 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-public class Solution {
+public class TimeConversion {
 
    public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
