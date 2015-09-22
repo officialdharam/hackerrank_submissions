e7f1a187509aae4bee13ff5276a9de3484217f02
Dharmendra Prasad <officialdharam@gmail.com>
2015-09-22 15:55:50 +0530

    Create ServiceLane.java
    Find all vehicles which can enter and exit in a given section.

---
+++ algorithms/implementation/ServiceLane.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int T = sc.nextInt();
+		int a[] = new int[N];
+		for (int i = 0; i < N; i++) {
+			a[i] = sc.nextInt();
+		}
+
+		for (int i = 0; i < T; i++) {
+			int x = sc.nextInt();
+			int y = sc.nextInt();
+			int min = Integer.MAX_VALUE;
+			for (int j = x; j <= y; j++) {
+				if (a[j] < min)
+					min = a[j];
+			}
+			System.out.println(min);
+		}
+	}
+}
