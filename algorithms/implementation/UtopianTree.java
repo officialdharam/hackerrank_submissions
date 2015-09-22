50edf463c6f30a71bd368f1e16ecf4791932e40c
Dharmendra Prasad <officialdharam@gmail.com>
2015-09-22 15:52:59 +0530

    Create UtopianTree.java
    Tree has 2 growth cycles in a year. 1st cycle = 2x , second cycle = +1

---
+++ algorithms/implementation/UtopianTree.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for (int i = 0; i < n; i++) {
+			int m = sc.nextInt();
+			int l = 1;
+			for (int j = 0; j < m; j++) {
+				if (j % 2 == 0)
+					l *= 2;
+				else
+					l += 1;
+			}
+			System.out.println(l);
+		}
+	}
+}
