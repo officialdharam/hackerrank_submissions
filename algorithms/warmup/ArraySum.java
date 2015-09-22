eb507cd1d802eb6991151a995c9d64a23619cd5d
Dharmendra Prasad <officialdharam@gmail.com>
2015-09-22 16:14:26 +0530

    Create ArraySum.java

---
+++ algorithms/warmup/ArraySum.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+        for(int i = 0 ; i < n; i++){
+            sum+=sc.nextInt();
+        }
+        System.out.println(sum);
+    }
+}
