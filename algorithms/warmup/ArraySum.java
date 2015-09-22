659c73a473bb9539831f0df5f28677f44a7adbc2
Dharmendra Prasad <officialdharam@gmail.com>
2015-09-22 16:17:47 +0530

    Update ArraySum.java

--- algorithms/warmup/ArraySum.java
+++ algorithms/warmup/ArraySum.java
@@ -4,7 +4,7 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-public class Solution {
+public class ArraySum {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
