78e2254f16658f87a6dd0f5797ae887a56ec354c
Dharmendra Prasad <officialdharam@gmail.com>
2015-09-22 15:52:05 +0530

    Create SherlockAndBeast.java
    Finding largest number with 5's and 3's . number of 5's must be divisible by 3 and vice versa

---
+++ algorithms/implementation/SherlockAndBeast.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for (int i = 0; i < n; i++) {
+			int N = sc.nextInt();
+			int pivot = getPivot(N);
+			if(pivot < 0)
+				System.out.println(-1);
+			else {
+				char[] c = new char[N];
+				int j = 0;
+				for (; j < pivot; j++) {
+					c[j] = '5';
+				}
+				for (; j < N; j++) {
+					c[j] = '3';
+				}
+				System.out.println(new String(c));
+			}
+		}
+	}
+
+	static int getPivot(int N) {
+		while (N > 0) {
+			if (N % 3 == 0)
+				break;
+			else {
+				N -= 5;
+			}
+		}
+		return N;
+	}
+}
