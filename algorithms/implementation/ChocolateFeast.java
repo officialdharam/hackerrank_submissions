19a5d13526cd1efc4cf0317c52f949ae53098185
Dharmendra Prasad <officialdharam@gmail.com>
2015-09-22 15:57:14 +0530

    Create ChocolateFeast.java
    Buy chocolates using wrappers and money.

---
+++ algorithms/implementation/ChocolateFeast.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int t = in.nextInt();
+		for (int i = 0; i < t; i++) {
+			System.out.println(Solve(in.nextInt(), in.nextInt(), in.nextInt()));
+		}
+	}
+
+	private static long Solve(int N, int C, int M) {
+		int c = N / C;
+		int w = c;
+		int k = 0;
+		while (w >= M) {
+			k = 0;
+			k = w / M;
+			w = (w % M) + k;
+			c += k;
+		}
+		return c;
+	}
+    
+    
+}
