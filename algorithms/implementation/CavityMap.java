a799864f415b9b242975780a22dc877b306c624f
Dharmendra Prasad <officialdharam@gmail.com>
2015-09-22 16:20:32 +0530

    Update CavityMap.java

--- algorithms/implementation/CavityMap.java
+++ algorithms/implementation/CavityMap.java
@@ -4,7 +4,7 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-public class Solution {
+public class CavityMap {
 
     public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
