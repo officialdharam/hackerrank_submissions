96cf893dc783718498f784c64cf3120399a1dfe7
Dharmendra Prasad <officialdharam@gmail.com>
2015-09-22 16:21:02 +0530

    Update Encryption.java

--- algorithms/implementation/Encryption.java
+++ algorithms/implementation/Encryption.java
@@ -4,7 +4,7 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-public class Solution {
+public class Encryption {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String S = sc.next();
