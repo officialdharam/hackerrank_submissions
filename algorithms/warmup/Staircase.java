685d57d2c02766e40584642e000f6cae4c8758fa
Dharmendra Prasad <officialdharam@gmail.com>
2015-09-22 16:16:02 +0530

    Create Staircase.java

---
+++ algorithms/warmup/Staircase.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		char[] c = new char[n];
+		Arrays.fill(c, ' ');
+		String s = null;
+		for (int i = 0; i < n; i++) {
+			c[n - i-1] = '#';
+			s = new String(c);
+			System.out.println(s);
+			s = null;
+		}
+	}
+}
