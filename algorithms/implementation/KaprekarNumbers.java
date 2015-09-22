db5fec18732a9a96e63cb424a7eeb66ca0c06498
Dharmendra Prasad <officialdharam@gmail.com>
2015-09-22 16:06:51 +0530

    Create KaprekarNumbers.java
    This is to find all Kaprekar Numbers within a range

---
+++ algorithms/implementation/KaprekarNumbers.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int P = sc.nextInt();
+		int Q = sc.nextInt();
+		int count = 0;
+		for (long i = P; i <= Q; i++) {
+			long S = i * i;
+			String T = S + "";
+			int l = T.length();
+
+			String L = T.substring(0, l / 2);
+			String R = T.substring(l / 2, l);
+
+			int LInt = 0;
+			int RInt = 0;
+			try {
+				LInt = Integer.parseInt(L);
+			} catch (Exception e) {
+
+			}
+			try {
+				RInt = Integer.parseInt(R);
+			} catch (Exception e) {
+
+			}
+			if (LInt + RInt == i) {
+				if (count > 0){
+					System.out.print(" ");
+				}
+				count++;
+				System.out.print(i);
+			}
+		}
+		if(count == 0)
+			System.out.println("INVALID RANGE");
+	}
+}
