18c90d94ed93b6916b1d7f0e1456912107c88c20
Dharmendra Prasad <officialdharam@gmail.com>
2015-09-22 15:54:02 +0530

    Create FindDigits.java
    Digits in the given number which exactly divides the number

---
+++ algorithms/implementation/FindDigits.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for (int i = 0; i < n; i++) {
+			int m = sc.nextInt();
+			int N = m;
+			int count = 0;
+			while (m > 0) {
+				int r = m % 10;
+				if (r != 0 && N % r == 0) {
+					count++;
+				}
+				m = m / 10;
+			}
+			System.out.println(count);
+		}
+
+	}
+}
