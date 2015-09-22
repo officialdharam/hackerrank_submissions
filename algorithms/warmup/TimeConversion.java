d37efa3e8c8368e9b861a98f43467292dc7c479d
Dharmendra Prasad <officialdharam@gmail.com>
2015-09-22 16:16:21 +0530

    Create TimeConversion.java

---
+++ algorithms/warmup/TimeConversion.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+   public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String time = sc.next();
+		DateFormat TWELVE_TF = new SimpleDateFormat("hh:mm:ssa");
+		DateFormat TWENTY_FOUR_TF = new SimpleDateFormat("HH:mm:ss");
+		try {
+			
+			System.out.println(TWENTY_FOUR_TF.format(TWELVE_TF.parse(time)));
+		} catch (ParseException e) {
+		}
+		System.exit(0);
+	}
+}
